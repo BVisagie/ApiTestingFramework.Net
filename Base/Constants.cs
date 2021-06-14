c6091e48d11ac0fc181be8505b9c3398f88c360a
Bernard <bernard.visagie@kingprice.co.za>
2021-06-14 15:16:21 +0200

    Imrpovement to Constants.cs

--- Base/Constants.cs
+++ Base/Constants.cs
@@ -2,10 +2,10 @@
 {
     public static class Constants
     {
-        public const string EnvironmentUnderTest = "EnvironmentUnderTest";
-        public const string EnvironmentUnderTestDomain = "EnvironmentUnderTestDomain";
-        public const string EnvironmentUnderTestPort = "EnvironmentUnderTestPort";
-        public const string ExampleDb = "ExampleDb";
-        public const string ExampleDbServer = "ExampleDbServer";
+        public const string EnvironmentUnderTest = nameof(EnvironmentUnderTest);
+        public const string EnvironmentUnderTestDomain = nameof(EnvironmentUnderTestDomain);
+        public const string EnvironmentUnderTestPort = nameof(EnvironmentUnderTestPort);
+        public const string ExampleDb = nameof(ExampleDb);
+        public const string ExampleDbServer = nameof(ExampleDbServer);
     }
 }
