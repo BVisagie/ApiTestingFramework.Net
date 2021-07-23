c280bea69f296ea545c75fbf4253bdeec50a9bd4
Bernard <bernard.visagie@kingprice.co.za>
2021-07-23 08:26:24 +0200

    Implemented GenericServiceRequests example

---
+++ ServicesLogic/GenericServiceRequests.cs
@@ -0,0 +1,83 @@
+﻿using ApiTestingFramework.Net.Base;
+using ApiTestingFramework.Net.ServicesLogic.Entities;
+using ApiTestingFramework.Net.Utilities;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+
+namespace ApiTestingFramework.Net.ServicesLogic
+{
+    public class GenericServiceRequests
+    {
+        private readonly TestInstance testInstance;
+
+        public GenericServiceRequests(TestInstance testInstance)
+        {
+            this.testInstance = testInstance;
+        }
+
+        public T CallTargetServiceWithARequestAndDeserializeResponse<T>(string targetEndpoint, object requestObject = null, Method httpMethod = Method.POST)
+        {
+            testInstance.Logger.Debug("Target Endpoint: {TargetEndpoint}", targetEndpoint);
+            testInstance.Logger.Debug("Request: {@RequestObject}", requestObject);
+
+            var request = new GenericHttpRequestRestSharp
+            {
+                ServiceUrl = targetEndpoint,
+                HttpMethod = httpMethod
+            };
+
+            if (requestObject != null)
+            {
+                request.RequestBody = requestObject;
+            }
+
+            IRestResponse response = new HttpServiceInteractions().HttpInteraction(request);
+
+            if (response.IsSuccessful)
+            {
+                var deserializedResponse = JsonConvert.DeserializeObject<T>(response.Content);
+                testInstance.Logger.Debug("Response: {@DeserializeResponse}", deserializedResponse);
+
+                return deserializedResponse;
+            }
+            else
+            {
+                new ApiTestLogger(testInstance).LogGenericIRestResponseError(request, restResponse: response);
+                return default;
+            }
+        }
+
+        public List<T> CallTargetServiceWithARequestAndDeserializeResponseAsList<T>(string targetEndpoint, object requestObject = null, Method httpMethod = Method.POST)
+        {
+            testInstance.Logger.Debug("Target Endpoint: {TargetEndpoint}", targetEndpoint);
+            testInstance.Logger.Debug("Request: {@RequestObject}", requestObject);
+
+            var request = new GenericHttpRequestRestSharp
+            {
+                ServiceUrl = targetEndpoint,
+                HttpMethod = httpMethod
+            };
+
+            if (requestObject != null)
+            {
+                request.RequestBody = requestObject;
+            }
+
+            IRestResponse response = new HttpServiceInteractions().HttpInteraction(request);
+
+            if (response.IsSuccessful)
+            {
+                var deserializedResponse = JsonConvert.DeserializeObject<List<T>>(response.Content);
+                testInstance.Logger.Debug("Response: {@DeserializeResponse}", deserializedResponse);
+
+                return deserializedResponse;
+            }
+            else
+            {
+                new ApiTestLogger(testInstance).LogGenericIRestResponseError(request, restResponse: response);
+                return default;
+            }
+        }
+    }
+}
---
+++ Utilities/ApiTestLogger.cs
@@ -0,0 +1,27 @@
+﻿using ApiTestingFramework.Net.Base;
+using ApiTestingFramework.Net.ServicesLogic.Entities;
+using RestSharp;
+
+namespace ApiTestingFramework.Net.Utilities
+{
+    public class ApiTestLogger
+    {
+        private readonly TestInstance testInstance;
+
+        public ApiTestLogger(TestInstance testInstance)
+        {
+            this.testInstance = testInstance;
+        }
+
+        public void LogGenericIRestResponseError(GenericHttpRequestRestSharp genericHttpRequestRestSharp, IRestResponse restResponse)
+        {
+            testInstance.Logger.Error($"An exception has been encountered while trying to contact the requested service URL: {genericHttpRequestRestSharp.ServiceUrl}");
+            testInstance.Logger.Error($"Response status: {restResponse.ResponseStatus}");
+            testInstance.Logger.Error($"Response status code: {restResponse.StatusCode}");
+            testInstance.Logger.Error($"Response status description: {restResponse.StatusDescription}");
+            testInstance.Logger.Error($"Response content: {restResponse.Content}");
+            testInstance.Logger.Error($"Response error message: {restResponse.ErrorMessage}");
+            testInstance.Logger.Error($"Response error exception: {restResponse.ErrorException}");
+        }
+    }
+}
