b1f4c15a66ffd5789db120000bab2657b670a766
Bernard <bernard.visagie@kingprice.co.za>
2021-07-23 08:15:49 +0200

    Tweak

--- Base/TestInstance.cs
+++ Base/TestInstance.cs
@@ -12,8 +12,8 @@ namespace ApiTestingFramework.Net.Base
         public string EnvironmentUnderTestPort { get; set; }
         public string ExampleDbServer { get; set; }
         public string ExampleDb { get; set; }
-        public SqlCredential SqlCredential { get; set; }
         public bool UseSqlCredential { get; set; }
+        public SqlCredential SqlCredential { get; set; }
         public string SqlUsername { get; set; }
         public string SqlPassword { get; set; }
         public bool MultiSubnetFailover { get; set; }
